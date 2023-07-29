16b9a253175ab969373321858a82d97568a75119
abdullahhhhhhhhhhhhhh <140548771+abdullahhhhhhhhhhhhhh@users.noreply.github.com>
2023-07-29 21:08:28 +0600

    Create 1046.c

---
+++ 1046.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int s,e;
+    scanf("%d%d",&s,&e);
+    if(s<e)
+    {
+        printf("O JOGO DUROU %d HORA(S)\n",e-s);
+    }
+    else if(s==e)
+    {
+        printf("O JOGO DUROU 24 HORA(S)\n");
+    }
+    else if(s>e)
+    {
+        printf("O JOGO DUROU %d HORA(S)\n",24-s+e);
+    }
+    return 0;
+}
