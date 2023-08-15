601e4183bd8168486916b1478f663b49b2359a05
abdullahhhhhhhhhhhhhh <140548771+abdullahhhhhhhhhhhhhh@users.noreply.github.com>
2023-08-16 00:55:08 +0600

    Create 1095.c

---
+++ 1095.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int I,J,n;
+    int i = 1;
+    int j = 60;
+    while(i<=38)
+ {
+     printf("I=%d J=%d\n",i,j);
+     i+=3;
+     j-=5;
+ }
+    return 0;
+}
+
